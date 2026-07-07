61d56ab5090047aa773c233b8369af801c279301
Vansh Pal <nk555pal@gmail.com>
2026-07-07 18:28:27 +0530

    Time: 5 ms (7.41%), Space: 10.6 MB (11.36%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.cpp
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -2,18 +2,13 @@ class Solution {
 public:
     bool isPalindrome(string s) {
         string str = "";
-        if(1<= s.length() && s.length() <= 200000){
-            for (char &i : s){
-                if (isalnum(i)){
-                    str += char(tolower(i));
-                }
+        for (char &i : s){
+            if (isalnum(i)){
+                str += char(tolower(i));
             }
-            string rev = str;
-            reverse(rev.begin(), rev.end());
-            return str == rev;
         }
-        else{
-            return false;
-        }
+        string rev = str;
+        reverse(rev.begin(), rev.end());
+        return str == rev;
     }
 };
